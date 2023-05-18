453ff54d7eb8e56bd9824b426c9e88d688f2779f
Rafaela Gomes <99452621+gsrafaela@users.noreply.github.com>
2023-05-18 10:55:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Program 
+{
+
+  public static void Main(string[] args) 
+  {
+    string[] entrada = Console.ReadLine().Split();
+    int participantes = int.Parse(entrada[0]);
+    int cachorrosQuentes = int.Parse(entrada[1]);
+
+    double media = (double) cachorrosQuentes / participantes;
+    
+    Console.WriteLine($"{media:F2}");
+  }
+}
+
